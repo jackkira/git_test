766ec82f4f3c21aeaf7036c750ff0075dfab3755
Jack <rupala.jaydeep@gmail.com>
2023-06-18 23:32:53 +0530

    Add more files

---
+++ read_df.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df_2 = pd.read_csv('C:\Users\Jaydeep  Rupala\Desktop\emp.csv')
+
+print(df_2.head())
---
+++ transform_df.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df_3 = pd.read_csv('C:\Users\Jaydeep  Rupala\Desktop\emp.csv')
+
+print(df_3.head())
